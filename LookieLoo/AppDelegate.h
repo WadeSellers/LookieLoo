9127b26dc02a54ee292d3a3722eddc11a4c3e4cf
Wade Sellers <Waderoni@gmail.com>
2014-10-16 18:03:29 -0500

    Initial Commit

---
+++ LookieLoo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LookieLoo
+//
+//  Created by Wade Sellers on 10/16/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LookieLoo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LookieLoo
+//
+//  Created by Wade Sellers on 10/16/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
